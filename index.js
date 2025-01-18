ccd78d2991e2ec4bebf5b7603269f5571054f6fc
Alexdudley95 <alexander.dudley95@gmail.com>
2025-01-18 21:34:19 +0000

    consolidated update to one output,  removed un-needed JSON check, added restart button

--- index.js
+++ index.js
@@ -7,122 +7,107 @@
 // Output value sends to the specific array within Choices, three choices are then updated from this array
 
 var inputVal = 0;
+var choice = 0;
 var outputVal = 0;
 var output = $("#output");
 var keyCollected = false;
 
 //set first choices & output label
-$(document).ready(function(){
-  $.getJSON("main.json", function(data){
-    $("#first").text(data.choices[0][0].choice);
-    $("#second").text(data.choices[0][1].choice);
-    $("#third").text(data.choices[0][2].choice);
-    $('#output').text(data.outputs[0].output);
+function setup(){
+  $(document).ready(function(){
+    $.getJSON("main.json", function(data){
+      $("#first").text(data.choices[0][0].choice);
+      $("#second").text(data.choices[0][1].choice);
+      $("#third").text(data.choices[0][2].choice);
+      $('#output').text(data.outputs[0].output);
+      $('#values').text("input: "+ inputVal + " choice: " + choice + " outputVal: " + outputVal);
 
-  }).fail(function(){
-      console.log("An error has occurred.");
+    }).fail(function(){
+        console.log("An error has occurred.");
+    });
   });
+}
+
+var restart = $("#restart");
+
+restart.on("click", function(){
+  setup();
+  console.log("hi");
 });
+setup();
 
 //enter & submit button event handlers
 
 $(document).on('keypress', function(e){
   if(e.which == '13'){
     var input = $("#input").val();
-    // checkActions();
+    checkActions();
     actionSubmitted(input);
     $("#input").val("");
-
+    $('#values').text("input: "+ inputVal + " choice: " + choice + " outputVal: " + outputVal);
   }
 })
 
-// var submit = $("#inputBut");
 
-// submit.on("click", function(){
-//   var input = $("#input").val();
-//   actionSubmitted(input);
-//   $("#input").val("");
-//   checkActions();
-// });
-
-// // function checkActions(){
-// //   let first = $("first").html();
-// //   console.log("here");
-// //   if (first == "Go into hut" && keyCollected == true){
-// //     console.log("inside");
-// //     $("#keyLabel").text("You used the key");
-// //   }
+function checkActions(){
+  let first = $("#first").html();
+  let check = "go into hut"
+  console.log("here");
+  if (!check.localeCompare(first, undefined, {sensitivity:'accent'}) && keyCollected){
+    console.log("inside");
+    $("#keyLabel").text("You used the key");
+  }
+}
 
 
-// }
+//set the text and update choices based on text
+
+function updateOutput(inVal, choiceVal, outVal){
+
+  $.getJSON("main.json", function(data){
+    outputVal = data.choices[inVal][choiceVal].value;
+    inputVal = data.outputs[outputVal].value;
+    output.text(data.outputs[outputVal].output);
+
+    //update choices
+    $("#first").text(data.choices[inputVal][0].choice);
+    $("#second").text(data.choices[inputVal][1].choice);
+    $("#third").text(data.choices[inputVal][2].choice);
+
+
+
+  }).fail(function(){
+    console.log("An error has occurred.");
+  });
+
+}
+
+
 
 function actionSubmitted(input){
   let first = $("#first").html();
   let second = $("#second").html();
   let third = $("#third").html();
-
   //compare input to current actions
-  $.getJSON("main.json", function(data){
 
     if(!input.localeCompare(first, undefined, {sensitivity:'accent'})){
+      choice = 0;
       if(first == "Collect key and return" && !keyCollected){
         keyCollected = true;
         console.log("Key collected");
         $("#keyLabel").text("You have a key");
-        outputVal = data.choices[inputVal][0].value;
-        output.text(data.outputs[outputVal].output);
-        inputVal = data.outputs[outputVal].value;
-        updateSelector(inputVal);
+        updateOutput(inputVal, choice, outputVal);
 
       }else{
-        outputVal = data.choices[inputVal][0].value;
-        output.text(data.outputs[outputVal].output);
-        inputVal = data.outputs[outputVal].value;
-        updateSelector(inputVal);
+        updateOutput(inputVal, choice, outputVal);
       }
     }else if (!input.localeCompare(second, undefined, {sensitivity:'accent'})){
-      outputVal = data.choices[inputVal][1].value;
-      output.text(data.outputs[outputVal].output);
-      inputVal = data.outputs[outputVal].value;
-      updateSelector(inputVal);
+      choice = 1;
+      updateOutput(inputVal, choice, outputVal);
 
     }else if(!input.localeCompare(third, undefined, {sensitivity:'accent'})){
-
-      outputVal = data.choices[inputVal][2].value;
-      output.text(data.outputs[outputVal].output);
-      inputVal = data.outputs[outputVal].value;
-      updateSelector(inputVal);
+      choice = 2;
+      updateOutput(inputVal, choice, outputVal);
 
     }
-  }).fail(function(){
-    console.log("An error has occurred.");
-  });
 }
-
-//set the choices
-function updateSelector(inputVal){
-
-    $.getJSON("main.json", function(data){
-      $("#first").text(data.choices[inputVal][0].choice);
-      $("#second").text(data.choices[inputVal][1].choice);
-      $("#third").text(data.choices[inputVal][2].choice);
-    }).fail(function(){
-        console.log("An error has occurred.");
-    });
-  }
-
-//set the text and update choices based on text
-//this seems to break everything at the moment
-  function updateOutput(inputVal, choice, outputVal){
-    $("#values").text(" " + inputVal + " " + choice + " " + outputVal);
-
-    $.getJSON("main.json", function(data){
-      outputVal = data.choices[inputVal][choice].value;
-      output.text(data.outputs[outputVal].output);
-      inputVal = data.outputs[outputVal].value;
-      updateSelector(inputVal);
-    }).fail(function(){
-      console.log("An error has occurred.");
-    });
-
-  }
